121fefb505363e487d6a1c577ce37385ea943e13
Lucas Correia <correia23.lucas@gmail.com>
2021-05-01 20:58:07 -0300

    page bpofiscal add

---
+++ src/pages/Servicos/BPOFiscal/components/FaqBPOFiscal/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Accordion from '../../../../../components/Accordion';
+
+function FaqBPOFiscal() {
+    return (
+        <Accordion idFaq='faqBPOFiscal'>
+            {[
+                { title: 'Duvida 1', description: 'Resposta breve de até 3 linhas', cardId: 'a', collapseId: 'collapseA', expanded: 'false' },
+                { title: 'Duvida 2', description: 'Resposta breve de até 3 linhas', cardId: 'b', collapseId: 'collapseB', expanded: 'false' },
+                { title: 'Duvida 3', description: 'Resposta breve de até 3 linhas', cardId: 'c', collapseId: 'collapseC', expanded: 'false' }
+            ]}
+        </Accordion>
+    );
+}
+
+export default FaqBPOFiscal;
