e231e2ca892a7dfc25531c6ad9b10ffd5b5174e4
Lucas Correia <correia23.lucas@gmail.com>
2021-05-01 20:57:47 -0300

    page bpofolha add

---
+++ src/pages/Servicos/BPOFolha/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Default from '../../../components/DefaultComponents';
+
+import MainBanner from '../../../components/BannerLevelTwo';
+import Detalhesservicos from './components/detalhesServicos';
+import Contrate from '../../../components/Contrate';
+import Faq from './components/FaqBPOFolha';
+
+function BPOContabil() {
+    return (
+        <Default>
+            <MainBanner
+                titulo="BPO Folha"
+                subtitulo="Confira os serviços relacionados á dpto. pessoal"
+                url="/formulario/"
+                ctaButton="Contrate a CLF"
+            />
+            <Detalhesservicos />
+            <Contrate />
+            <Faq />
+        </Default>
+    );
+}
+
+export default BPOContabil;
